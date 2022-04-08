a72716a751c8d921af27abcad9f0467366730e36
Ruan Pablo <ruan_goio10@hotmail.com>
2022-04-07 22:06:59 -0300

    :scroll: Check inputs

--- src/__tests__/screens/profile.test.tsx
+++ src/__tests__/screens/profile.test.tsx
@@ -6,6 +6,18 @@ import { Profile } from '../../../src/screens/Profile';
 
 test('check if show correctly user input name placeholder', () => {
     const { getByPlaceholderText } = render(<Profile />);
-    const input = getByPlaceholderText('Nome');
-    expect(input).toBeTruthy();
-})
+    const inputName = getByPlaceholderText('Nome');
+    expect(inputName.props.placeholder).toBeTruthy();
+});
+
+test('check if show correctly user input lastname placeholder', () => {
+    const { getByPlaceholderText } = render(<Profile />);
+    const inputLastname = getByPlaceholderText('Sobrenome');
+    expect(inputLastname.props.placeholder).toBeTruthy();
+});
+
+test('check if exist save button', () => {
+    const { getByText } = render(<Profile />);
+    const saveButton = getByText('Salvar');
+    expect(saveButton).toBeTruthy();
+});
